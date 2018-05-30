ec66155126a8f1db1fc6f44a1824ebcb367c5c62
Amber <amber__12@hotmail.com>
2018-05-30 15:56:44 +0200

    Starting up project

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Smart Planner</title>
+</head>
+<body>
+
+<input type=button onClick="parent.location='settings.php'" value="settings"/>
+    
+</body>
+</html>
---
+++ settings.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <title>Smart Planner</title>
+</head>
+<body>
+
+<form>
+    Start tijd: 
+    <br>
+    <input type="time" name="starting-time"/>
+    <br>
+    <br>
+    Eind tijd: 
+    <br>
+    <input type="time" name="ending-time"/>
+    <br>
+    <br>
+    <input type="submit" value="Opslaan"/>
+</form>
+    
+</body>
+</html>
