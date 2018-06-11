001030919a7ff2d74b42d712315a435b6b52fdf2
Amber <amber__12@hotmail.com>
2018-06-11 14:18:17 +0200

    Added form for event adding

--- index.php
+++ index.php
@@ -6,6 +6,27 @@
 </head>
 <body>
 
+<form action="addEvent.php" method="post">
+    To do:
+    <br>
+    <input type="text" name="task"/>
+    <br>
+    <br>
+    Werktijd per dag:
+    <br>
+    <input type="time" name="working-time"/>
+    <br>
+    <br>
+    Deadline:
+    <br>
+    <input type="datetime-local" name="deadline"/>
+    <br>
+    <br>
+    <input type="submit" value="Taak toevoegen"/>
+</form>
+
+<br>
+<br>
 <input type=button onClick="parent.location='settings.php'" value="settings"/>
 <input type=button onClick="parent.location='quickstart.php'" value="Get events"/>
     
